78dd3e262ecab026674bb1bf6cb905ef3a36f32c
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2026-02-13 21:05:20 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// console.log("salam");
+// console.log("hello");
+
+// console.warn("intenet zeifdir");
+// console.error("internet islemir");
+
+// console.clear();
+// console.log("salam");
+
+// console.log("hsj");
+
+// console.log("My name is Aygun");
+
+// console.log("Aygun");
+
+// var
+// let
+// const
+
+var username = "Xedice";
+// var username = "Esmer";
+// console.log(username + " Memmedov");
+console.log(username);
+
+// let nickname = "Nicat";
+// console.log(nickname);
+
+// const student =  "Guler";
+// console.log(student);
