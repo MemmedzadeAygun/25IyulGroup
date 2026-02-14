105fc9c0c7b01e9fa779bd607ff4cfee6fa58202
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2026-02-14 14:33:14 +0400

    variable and number

--- app.js
+++ app.js
@@ -17,13 +17,49 @@
 // let
 // const
 
-var username = "Xedice";
+// var username = "Xedice";
 // var username = "Esmer";
 // console.log(username + " Memmedov");
-console.log(username);
+// console.log(username);
+
+// let teacher;
+// teacher = "Eldar";
+// console.log(teacher);
+
 
 // let nickname = "Nicat";
+// nickname = "Omer";
+// let nickname = "Eldar";
 // console.log(nickname);
 
 // const student =  "Guler";
+// student = "Nigar";
 // console.log(student);
+
+// const user = "Mehemmed";
+// user = "Gunay";
+// console.log(user);
+
+// let myNewAge = 12;
+// console.log(myNewAge);
+
+// const name = "Mehriban";
+// console.log(name);
+
+// let a = 10;
+// let b = 30;
+// let x = a + b;
+// console.log(x);
+// console.log(b - a);
+// console.log(a * b);
+// console.log(b / a);
+// console.log(a + 10);
+// let c = a + 30;
+// console.log(c);
+
+// let number = 22;
+// let number = 22.23;
+// let number = -22;
+// console.log(number);
+
+// let serh = "salam";
