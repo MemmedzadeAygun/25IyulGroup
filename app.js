047493bd6723eb09f532d8a9c5bf9e1c6cf6768f
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2026-02-18 16:05:16 +0400

    type of the variable

--- app.js
+++ app.js
@@ -63,3 +63,70 @@
 // console.log(number);
 
 // let serh = "salam";
+
+// let text = "Aygun";
+// console.log(text);
+
+// let num = "50";
+// console.log(num);
+
+// let number = 60;
+// console.log(number);
+
+// let result = true;
+// let result = false;
+// console.log(result);
+
+// let f;
+// console.log(f);
+
+// let a = null;
+// console.log(a);
+
+// let a = 29;
+// console.log(a ** 3);
+// console.log(a / 5);
+// console.log(a % 5);
+// a = a + 5;
+// a += 5;
+// a = a - 5; a -= 5;
+// a = a + 1;
+// a += 1;
+// a++;
+// ++a;
+// a *= 2;
+// a /= 2;
+// a--;
+// --a;
+// console.log(a + 5);
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+
+// let num  = 30;
+
+// let num = 5;
+// console.log(num ** 5);
+// console.log(num % 3);
+
+// let a = 12;
+// console.log(a / 0);
+// console.log(a * "salam");
+
+// let name = "Eli";
+// let surname = "Qedimov";
+// console.log(name);
+// console.log(surname);
+
+// console.log(name + " " + surname);
+// let fullName = name + " " + surname;
+// console.log(fullName);
+
+// console.log(name, surname);
+
+// let userInfo = "Elinin kitabi";
+// let userInfo = "Eli's book";
+// let userInfo = 'Eli"s book';
+// let userInfo = 'Eli\'s book';
+// console.log(userInfo);
+
